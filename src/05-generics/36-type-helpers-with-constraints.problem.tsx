79d84a1bc44eac068d5d4dd7ba32bcb919dcf053
m-keshavarz <m.k.dev@outlook.com>
2024-03-30 17:07:43 +0330

    add type helpers with constraints

--- src/05-generics/36-type-helpers-with-constraints.problem.tsx
+++ src/05-generics/36-type-helpers-with-constraints.problem.tsx
@@ -1,14 +1,23 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type AllOrNothing<T> = T | ToUndefinedObject<T>;
+type AllOrNothing<T extends Record<string, unknown>> = T | ToUndefinedObject<T>;
 
-type ToUndefinedObject<T> = Partial<Record<keyof T, undefined>>;
+type ToUndefinedObject<T extends Record<string, unknown>> = Partial<
+  Record<keyof T, undefined>
+>;
 
 /**
  * There's a problem with our AllOrNothing type. It's letting
  * us pass ANYTHING as T. We want to constrain it so that it
  * only works with objects.
  */
+
+const vAny: any = 10;
+let vUnknown: unknown = 10;
+
+const testAny: string = vAny;
+if (typeof vUnknown === "string") vUnknown.split("");
+if (typeof vUnknown === "number") vUnknown++;
 
 type tests = [
   // @ts-expect-error
