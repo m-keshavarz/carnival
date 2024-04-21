0d9447cc5d4833117f61bcdd9893587ead4a2941
m-keshavarz <m.k.dev@outlook.com>
2024-04-21 21:42:46 +0330

    start tuples

--- src/06-advanced-hooks/45-tuple-return-type.problem.ts
+++ src/06-advanced-hooks/45-tuple-return-type.problem.ts
@@ -12,14 +12,14 @@ import { Equal, Expect } from "../helpers/type-utils";
  */
 
 export const useId = (defaultId: string) => {
-  const [id, setId] = useState(defaultId);
+  const [id, setId] = useState<string>(defaultId);
 
-  return [id, setId];
+  return [id, setId] as const
 };
 
 const [id, setId] = useId("1");
 
 type tests = [
   Expect<Equal<typeof id, string>>,
-  Expect<Equal<typeof setId, React.Dispatch<React.SetStateAction<string>>>>,
+  Expect<Equal<typeof setId, React.Dispatch<React.SetStateAction<string>>>>
 ];
